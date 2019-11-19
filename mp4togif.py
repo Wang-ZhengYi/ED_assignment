99f8bbfe2aa735fde56d79a2cf3bd87aea5c4942
Zhengyi Wang <zyw_hii@outlook.com>
2019-11-19 16:24:35 +0800

    gif

---
+++ mp4togif.py
@@ -0,0 +1,16 @@
+#!\usr\bin\python3
+# -*- coding: utf-8 -*-
+'''
+	Created on Nov. 2019
+	
+	Electric Dynamic Chapter 5
+
+	@author: ZYW @ BNU
+'''
+from moviepy.editor import *
+
+final_path = './videos/finalVideo.mp4'
+
+clip = (VideoFileClip(final_path).subclip(0,6.28).resize(0.2))
+
+clip.write_gif("E_field.gif") 
